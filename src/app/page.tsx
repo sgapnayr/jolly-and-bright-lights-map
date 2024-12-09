b5286d8b6629b965a23d613a22fe29fbea743142
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-12-09 14:16:28 -0600

    update to markers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@
 import markers from "@/app/data/markers.json";
 import Image from "next/image";
 
+// import Map from "@/components/Map";
 import dynamic from "next/dynamic";
 const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-gradient-to-b from-blue-100 to-white">
-      <header className="bg-gradient-to-r from-red-400 via-pink-500 to-purple-500 text-white py-6 shadow-lg">
+      <header className="z-50 bg-gradient-to-r from-red-400 via-pink-500 to-purple-500 text-white py-6 shadow-xl">
         <div className="container mx-auto text-center">
           <h1 className="text-2xl font-bold tracking-widest uppercase">
             Hosted by Jolly & Bright
@@ -29,7 +30,7 @@ export default function Home() {
         alt="Jolly & Bright Logo"
         width={128}
         height={128}
-        className="absolute z-50 md:bottom-8 bottom-16 right-4 rounded-lg shadow-xl"
+        className="absolute z-50 md:bottom-8 bottom-16 right-16 md:right-8 rounded-lg shadow-xl"
       />
     </div>
   );
