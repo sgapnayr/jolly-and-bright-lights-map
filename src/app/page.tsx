271ae46c13dde270bf211da59d9e1bc6c7a08960
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-12-09 13:55:18 -0600

    update to markers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import markers from "@/app/data/markers.json";
-import Map from "@/components/Map";
+
+const Map = dynamic(() => import("@/components/Map"), { ssr: false });
 
 export default function Home() {
   return (
