c4f4412fa55c4c7a40eb856a98c44900be7d1fc7
sgapnayr <100978214+sgapnayr@users.noreply.github.com>
2024-12-09 13:02:07 -0600

    my-sister-is-lucky-her-brother-is-an-enterprise-engineer

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -34,33 +34,35 @@ const Map: React.FC<MapProps> = ({ markers }) => {
   const center: LatLngExpression = [30.3183, -85.8561];
 
   return (
-    <MapContainer
-      center={center}
-      zoom={13}
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <TileLayer
-        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-        attribution='&copy; <a href="https://www.carto.com/">CARTO</a>'
-      />
-      {markers.map((marker) => (
-        <Marker
-          key={marker.id}
-          position={[marker.lat, marker.lng]}
-          icon={createCustomIcon(marker.photo)}
-        >
-          <Popup>
-            <h3>{marker.name}</h3>
-            <img
-              src={marker.photo}
-              alt={marker.name}
-              style={{ width: "100%" }}
-            />
-            <p>{marker.description}</p>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div className="map-wrapper">
+      <MapContainer
+        center={center}
+        zoom={13}
+        style={{ height: "calc(100vh - 100px)", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.carto.com/">CARTO</a>'
+        />
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            position={[marker.lat, marker.lng]}
+            icon={createCustomIcon(marker.photo)}
+          >
+            <Popup>
+              <h3>{marker.name}</h3>
+              <img
+                src={marker.photo}
+                alt={marker.name}
+                style={{ width: "100%" }}
+              />
+              <p>{marker.description}</p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,123 @@
-import Map from "@/app/components/Map";
-import markers from "@/app/data/markers.json";
+import Map from "./components/Map";
+import "./globals.css";
+
+const markers = [
+  {
+    id: 1,
+    name: "8701 Sandbar Ln, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 2,
+    name: "8675 Coral Reef Wy, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 3,
+    name: "8785 Coral Reef Wy, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 4,
+    name: "8660 Treasure Past Wy, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 5,
+    name: "8457 Dreams Float Ct, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 6,
+    name: "9219 Paradise Dr, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Place no longer exists",
+  },
+  {
+    id: 7,
+    name: "9339 Paradise Dr, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 8,
+    name: "8640 Treasure Past Wy, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 9,
+    name: "8713 Coral Reef Wy, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 10,
+    name: "8721 Seaplane Dr, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 11,
+    name: "8650 Treasure Past Wy, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 12,
+    name: "8489 Island Breeze Ave, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+  {
+    id: 13,
+    name: "8709 Sandbar Ln, Panama City Beach, FL 32413",
+    lat: 30.3183,
+    lng: -85.8561,
+    photo: "/lights.jpg",
+    description: "Beautiful lights with a reindeer display!",
+  },
+];
 
 export default function Home() {
-  return <Map markers={markers} />;
+  return (
+    <div>
+      {/* Header */}
+      <header className="header">
+        <h1>Hosted by Jolly & Bright</h1>
+      </header>
+
+      {/* Map */}
+      <Map markers={markers} />
+    </div>
+  );
 }
